dc7f303fdc2ea6511ca7db16a5c0129f23988c75
timonpalm <timon.palm@gmx.de>
2023-10-03 19:54:19 +0200

    conversion to next.js

---
+++ pages/index.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+function Header({ title }) {
+  return <h1>{title ? title : 'Default title'}</h1>;
+}
+
+export default function HomePage() {
+  const names = ["Timon", "Timon", "und", "Timon"];
+
+  const [likes, setLikes] = useState(0);
+
+  function handleClick() {
+    setLikes(likes + 1);
+  }
+
+  return (
+    <div>
+      <Header title="Develop. Preview. Ship. 🚀" />
+      <ul>
+        {names.map((name) => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+
+      <button onClick={handleClick}>Like ({likes})</button>
+    </div>
+  );
+}
